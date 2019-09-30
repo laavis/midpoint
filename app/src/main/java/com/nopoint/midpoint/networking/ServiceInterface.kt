7ed3f9b49c80908eca5e31a4112b52fdd9085eac
Niklas Kiuru <niklas.kiuru1@gmail.com>
2019-09-30 23:16:16 +0300

    #2 add function overloads for get/post requests with authorization

--- app/src/main/java/com/nopoint/midpoint/networking/ServiceInterface.kt
+++ app/src/main/java/com/nopoint/midpoint/networking/ServiceInterface.kt
@@ -4,6 +4,8 @@ import org.json.JSONObject
 
 interface ServiceInterface {
     fun post(api: API, path: String, params: JSONObject, completionHandler: (response: JSONObject?) -> Unit)
+    fun post(api: API, path: String, params: JSONObject, auth: String, completionHandler: (response: JSONObject?) -> Unit)
     fun get(api: API, path: String, completionHandler: (response: JSONObject?) -> Unit)
+    fun get(api: API, path: String, auth: String, completionHandler: (response: JSONObject?) -> Unit)
 
 }
--- app/src/main/java/com/nopoint/midpoint/networking/ServiceVolley.kt
+++ app/src/main/java/com/nopoint/midpoint/networking/ServiceVolley.kt
@@ -10,7 +10,12 @@ import org.json.JSONObject
 class ServiceVolley : ServiceInterface {
     val TAG = ServiceVolley::class.java.simpleName
 
-    override fun post(api: API, path: String, params: JSONObject, completionHandler: (response: JSONObject?) -> Unit) {
+    override fun post(
+        api: API,
+        path: String,
+        params: JSONObject,
+        completionHandler: (response: JSONObject?) -> Unit
+    ) {
         val url = "${api.path}$path"
         Log.d("URL", url)
         val jsonObjReq = object : JsonObjectRequest(Method.POST, url, params,
@@ -52,5 +57,58 @@ class ServiceVolley : ServiceInterface {
         BackendVolley.instance?.addToRequestQueue(jsonObjReq, TAG)
     }
 
+    override fun post(
+        api: API,
+        path: String,
+        params: JSONObject,
+        auth: String,
+        completionHandler: (response: JSONObject?) -> Unit
+    ) {
+        val url = "${api.path}$path"
+        Log.d("URL", url)
+        val jsonObjReq = object : JsonObjectRequest(Method.POST, url, params,
+            Response.Listener<JSONObject> { response ->
+                completionHandler(response)
+            },
+            Response.ErrorListener { error ->
+                VolleyLog.e(TAG, " ${error.message}")
+                completionHandler(null)
+            }) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Content-Type"] = "application/json"
+                headers["authorization"] = auth
+                return headers
+            }
+        }
+        BackendVolley.instance?.addToRequestQueue(jsonObjReq, TAG)
+    }
 
+    override fun get(
+        api: API,
+        path: String,
+        auth: String,
+        completionHandler: (response: JSONObject?) -> Unit
+    ) {
+        val url = "${api.path}$path"
+        val jsonObjReq = object : JsonObjectRequest(Method.GET, url, null,
+            Response.Listener<JSONObject> { response ->
+                Log.d(TAG, "/get request OK! Response: $response")
+                completionHandler(response)
+            },
+            Response.ErrorListener { error ->
+                VolleyLog.e(TAG, "/get request fail! Error: ${error.message}")
+                completionHandler(null)
+            }) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Content-Type"] = "application/json"
+                headers["authorization"] = auth
+                return headers
+            }
+        }
+        BackendVolley.instance?.addToRequestQueue(jsonObjReq, TAG)
+    }
 }
