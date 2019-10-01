3bd1d26e1f32aeafdcc760398e1cf1ec8b6a1340
laavis <sara.suviranta@gmail.com>
2019-10-01 21:21:18 +0300

    create search view

--- app/src/main/java/com/nopoint/midpoint/fragments/FriendsFragment.kt
+++ app/src/main/java/com/nopoint/midpoint/fragments/FriendsFragment.kt
@@ -1,27 +1,38 @@
 package com.nopoint.midpoint.fragments
 
 
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.nopoint.midpoint.MainActivity
-
 import com.nopoint.midpoint.R
 import kotlinx.android.synthetic.main.fragment_friends.*
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 /**
  * A simple [Fragment] subclass.
  */
 class FriendsFragment : Fragment() {
 
+    private lateinit var bottomNav: BottomNavigationView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val mainActivity = MainActivity()
+        val view = inflater.inflate(R.layout.fragment_friends, container, false)
+        bottomNav = (activity as MainActivity).findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
         (activity as MainActivity).supportActionBar?.title = "Friends"
 
-        return inflater.inflate(R.layout.fragment_friends, container, false)
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -29,6 +40,7 @@ class FriendsFragment : Fragment() {
 
         friends_fab_add.setOnClickListener {
             friends_fab_add.isExpanded = true
+
         }
 
         friends_add_return.setOnClickListener {
@@ -36,5 +48,13 @@ class FriendsFragment : Fragment() {
         }
     }
 
+    private fun setStatusBarColor(colorId: Int) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val ma = activity as MainActivity
+            val window = ma.window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = ContextCompat.getColor(ma, colorId)
+        }
+    }
 
 }
