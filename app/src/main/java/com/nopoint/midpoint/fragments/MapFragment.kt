68d33f24d26ff0a47590388d16224d142b9f5b07
laavis <sara.suviranta@gmail.com>
2019-10-03 14:08:42 +0300

    set origin coordinates to first latlng of the route when calculating midpoint

--- app/src/main/java/com/nopoint/midpoint/fragments/MapFragment.kt
+++ app/src/main/java/com/nopoint/midpoint/fragments/MapFragment.kt
@@ -1,6 +1,5 @@
 package com.nopoint.midpoint.fragments
 
-import android.graphics.Color
 import android.location.Location
 import android.os.Bundle
 import android.util.Log
@@ -31,9 +30,6 @@ import kotlinx.android.synthetic.main.fragment_map.*
 import com.google.android.gms.maps.model.LatLng
 
 
-
-
-
 /**
  * A "simple" [Fragment] subclass.
  */
@@ -53,10 +49,14 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     private var state = BottomSheetBehavior.STATE_COLLAPSED
 
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_map, container, false)
-        if (mapFragment == null){
+        if (mapFragment == null) {
             mapFragment = SupportMapFragment.newInstance()
             mapFragment!!.getMapAsync(this)
         }
@@ -69,20 +69,22 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     }
 
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         // For testing
         val espooLatLng = LatLng(60.205498, 24.653837)
-        val kauklahtiLatLng = LatLng(60.193327, 24.598813)
+        val kauklahtiLatLng = LatLng(60.193357, 24.598792)
+        val hese = LatLng(60.191205, 24.949999)
 
         val url = Directions.buildUrlTest(espooLatLng, kauklahtiLatLng)
+        val url2 = Directions.buildUrlTest(kauklahtiLatLng, hese)
+
 
         button_TEST.setOnClickListener {
-            Log.d("MIDPOINT", "url: $url")
+            Log.d("ROUTING", "url: $url")
 
-            apiController.get(API.DIRECTIONS, url) {response ->
+            apiController.get(API.DIRECTIONS, url2) { response ->
                 if (response != null) {
 
                     val result = Gson().fromJson(response.toString(), Direction::class.java)
@@ -90,16 +92,18 @@ class MapFragment : Fragment(), OnMapReadyCallback {
                     val legs = result.routes[0].legs
                     val path = ArrayList<LatLng>()
 
-                    for(i in 0 until legs[0].steps.size) {
+                    for (i in 0 until legs[0].steps.size) {
                         path.addAll(PolyUtil.decode(legs[0].steps[i].polyline.points))
                     }
 
+                    Log.d("ROUTING DISTANCE VALUE", "${legs[0].distance.value}")
 
                     val middle = (legs[0].distance.value / 2).toDouble()
 
-                    val midPointCoordinates = extrapolate(path, espooLatLng, middle)
+                    val midPointCoordinates = extrapolate(path, path[0], middle)
 
-                    val midpointURL = Directions.buildUrlTest(espooLatLng, midPointCoordinates)
+
+                    val midpointURL = Directions.buildUrlTest(kauklahtiLatLng, midPointCoordinates)
 
 
                     apiController.get(API.DIRECTIONS, midpointURL) { response ->
@@ -114,13 +118,49 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         }
     }
 
+    public fun decodePolyline(encoded: String): List<LatLng> {
 
-    // VITTU TÄMÄ NII
+        val poly = ArrayList<LatLng>()
+        var index = 0
+        val len = encoded.length
+        var lat = 0
+        var lng = 0
+
+        while (index < len) {
+            var b: Int
+            var shift = 0
+            var result = 0
+            do {
+                b = encoded[index++].toInt() - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlat = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lat += dlat
+
+            shift = 0
+            result = 0
+            do {
+                b = encoded[index++].toInt() - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlng = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lng += dlng
+
+            val latLng = LatLng((lat.toDouble() / 1E5), (lng.toDouble() / 1E5))
+            poly.add(latLng)
+        }
+
+        return poly
+    }
+
+
     private fun extrapolate(path: List<LatLng>, origin: LatLng, distance: Double): LatLng? {
         var extrapolated: LatLng? = null
 
-        if (!PolyUtil.isLocationOnPath(origin, path, false, 1.0)) {
-            Log.d("EXTRA", "something is going oooon")
+        if (!PolyUtil.isLocationOnPath(origin, path, false, 10.0)) {
+            return null
         }
 
         var accDistance = 0.0
@@ -139,29 +179,32 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             var currentDistance: Double = 0.0
 
             if (!foundStart) {
-                if(PolyUtil.isLocationOnPath(origin, segment, false, 1.0)) {
+
+                if (PolyUtil.isLocationOnPath(origin, segment, false, 1.0)) {
 
                     foundStart = true
 
                     currentDistance = SphericalUtil.computeDistanceBetween(origin, segmentEnd)
-                    Log.d("EXTRA DIST", "$currentDistance")
-                    Log.d("EXTRA CURRENT DIST", "$distance")
 
 
                     if (currentDistance > distance) {
-                        Log.d("EXTRA", "here")
 
                         val heading: Double = SphericalUtil.computeHeading(origin, segmentEnd)
-                        extrapolated = SphericalUtil.computeOffset(origin, distance - accDistance, heading)
+                        extrapolated =
+                            SphericalUtil.computeOffset(origin, distance - accDistance, heading)
                         break
                     }
                 }
+
+
             } else {
                 currentDistance = SphericalUtil.computeDistanceBetween(segmentStart, segmentEnd)
 
+
                 if (currentDistance + accDistance > distance) {
                     val heading: Double = SphericalUtil.computeHeading(segmentStart, segmentEnd)
-                    extrapolated = SphericalUtil.computeOffset(segmentStart, distance - accDistance, heading)
+                    extrapolated =
+                        SphericalUtil.computeOffset(segmentStart, distance - accDistance, heading)
                     break
                 }
             }
@@ -200,7 +243,8 @@ class MapFragment : Fragment(), OnMapReadyCallback {
                         val loc = LatLng(location.latitude, location.longitude)
                         Log.d("MAP", "lat: ${location.latitude}, lng: ${location.longitude}")
                         val cam = CameraUpdateFactory.newLatLngZoom(loc, 15.0f)
-                        val sheetFragment = childFragmentManager.findFragmentById(R.id.fragment) as MeetingFragment
+                        val sheetFragment =
+                            childFragmentManager.findFragmentById(R.id.fragment) as MeetingFragment
                         sheetFragment.currentLocation = location
                         mMap.animateCamera(cam)
                     }
@@ -230,7 +274,14 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         val startLatLng = LatLng(fullRoute.startLat!!, fullRoute.startLng!!)
         val startMarkerOptions: MarkerOptions =
             MarkerOptions().position(startLatLng).title(fullRoute.startName)
-                .icon(BitmapDescriptorFactory.fromBitmap(MapsFactory.drawMarker(this.activity!!, "")))
+                .icon(
+                    BitmapDescriptorFactory.fromBitmap(
+                        MapsFactory.drawMarker(
+                            this.activity!!,
+                            ""
+                        )
+                    )
+                )
         val endLatLng = LatLng(fullRoute.endLat!!, fullRoute.endLng!!)
 
         val endMarkerOptions: MarkerOptions =
@@ -267,7 +318,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     fun getDirections(destination: String = "", destinationCoord: Location?) {
         if (mRouteMarkerList.isNotEmpty()) clearMarkersAndRoute()
-        if (state == BottomSheetBehavior.STATE_EXPANDED){
+        if (state == BottomSheetBehavior.STATE_EXPANDED) {
             sheetBehavior!!.state = BottomSheetBehavior.STATE_COLLAPSED
         }
         fusedLocationClient?.lastLocation?.addOnSuccessListener { loc: Location? ->
@@ -285,22 +336,25 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     }
 
     // Bottom sheet
-    private fun createBottomSheetCb(): BottomSheetBehavior.BottomSheetCallback{
+    private fun createBottomSheetCb(): BottomSheetBehavior.BottomSheetCallback {
         return object : BottomSheetBehavior.BottomSheetCallback() {
             override fun onStateChanged(view: View, newState: Int) {
                 when (newState) {
                     BottomSheetBehavior.STATE_HIDDEN -> state = BottomSheetBehavior.STATE_HIDDEN
                     BottomSheetBehavior.STATE_EXPANDED -> state = BottomSheetBehavior.STATE_EXPANDED
-                    BottomSheetBehavior.STATE_COLLAPSED -> state = BottomSheetBehavior.STATE_COLLAPSED
+                    BottomSheetBehavior.STATE_COLLAPSED -> state =
+                        BottomSheetBehavior.STATE_COLLAPSED
                     BottomSheetBehavior.STATE_DRAGGING -> {
                         if (state != BottomSheetBehavior.STATE_HALF_EXPANDED) {
                             sheetBehavior!!.state = BottomSheetBehavior.STATE_HALF_EXPANDED
                         }
                     }
                     BottomSheetBehavior.STATE_SETTLING -> state = BottomSheetBehavior.STATE_SETTLING
-                    BottomSheetBehavior.STATE_HALF_EXPANDED -> state = BottomSheetBehavior.STATE_HALF_EXPANDED
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> state =
+                        BottomSheetBehavior.STATE_HALF_EXPANDED
                 }
             }
+
             override fun onSlide(view: View, v: Float) {}
         }
     }
