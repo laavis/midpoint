9c6cd0365bae36961ac47725bed10646888cb7e1
Niklas Kiuru <niklas.kiuru1@gmail.com>
2019-10-07 16:24:52 +0300

    Fix UI for bottom sheet recycler view

--- app/src/main/java/com/nopoint/midpoint/adapters/MeetingRequestsAdapter.kt
+++ app/src/main/java/com/nopoint/midpoint/adapters/MeetingRequestsAdapter.kt
@@ -1,22 +1,29 @@
 package com.nopoint.midpoint.adapters
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.card.MaterialCardView
 import com.nopoint.midpoint.R
 import com.nopoint.midpoint.models.*
 import kotlinx.android.synthetic.main.request_header.view.*
 import kotlinx.android.synthetic.main.request_row.view.*
+import org.jetbrains.anko.backgroundColor
+
 
 class MeetingRequestsAdapter(
     private val requests: MutableList<MeetingRequestRow>,
     private val context: Context,
-    private val respond: (meetingRequest: MeetingRequest) -> Unit,
-    private val showOnMap: (meetingRequest: MeetingRequest) -> Unit
+    private val listener: MeetingRequestViewListener
 ) : RecyclerView.Adapter<ViewHolder>() {
     override fun getItemCount(): Int = requests.size
 
@@ -32,31 +39,90 @@ class MeetingRequestsAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if (position == 0){
+            holder.topDivider?.visibility = View.GONE
+        }
         val request = requests[position]
         if (request.rowType != RowType.HEADER) {
-            when(request.type) {
-                MeetingType.REJECTED -> holder.userName!!.text = context.getString(R.string.meeting_rejected, request.meetingRequest!!.receiverUsername)
-                MeetingType.ACTIVE -> {
-                    if (request.rowType == RowType.DELETABLE) {
-                        holder.userName!!.text = context.getString(R.string.meeting_active, request.meetingRequest!!.receiverUsername)
-                    } else {
-                        holder.userName!!.text = context.getString(R.string.meeting_active, "You")
-                    }
-                    holder.meetBtn!!.text = context.getString(R.string.show_route)
-                    holder.meetBtn.setOnClickListener { showOnMap(request.meetingRequest!!) }
+            if (request.type == MeetingType.OUTGOING || request.type == MeetingType.INCOMING) {
+                holder.itemView.setOnClickListener {
+                    // Get the current state of the item
+                    val expanded = request.expanded
+                    // Change the state
+                    request.expanded = !expanded
+                    // Notify the adapter that item has changed
+                    notifyItemChanged(position)
                 }
-                MeetingType.INCOMING -> {
-                    holder.userName!!.text = context.getString(R.string.meeting_incoming, request.meetingRequest!!.requesterUsername)
-                    holder.meetBtn!!.setOnClickListener { respond(request.meetingRequest) }
+            } else if (request.type == MeetingType.ACTIVE) {
+                holder.itemView.setOnClickListener {
+                    listener.showOnMap(request.meetingRequest!!)
                 }
-                MeetingType.OUTGOING -> {
-                    holder.userName!!.text = context.getString(R.string.meeting_outgoing, request.meetingRequest!!.receiverUsername)
-                    holder.meetBtn!!.visibility = View.GONE
+            } else {
+                holder.itemView.setOnClickListener { }
+            }
+            holder.icon?.visibility = View.GONE
+            holder.secondaryButton?.visibility = View.GONE
+            bind(request, holder)
+        } else {
+            holder.headerTxt!!.text =
+                context.getString(R.string.meeting_heading, request.type.toString())
+        }
+    }
+
+    private fun bind(request: MeetingRequestRow, holder: ViewHolder) {
+        when (request.type) {
+            MeetingType.REJECTED -> {
+                if (request.rowType == RowType.DELETABLE) {
+                    holder.userName!!.text = context.getString(
+                        R.string.meeting_rejected,
+                        request.meetingRequest!!.receiverUsername
+                    )
+                } else {
+                    holder.userName!!.text = context.getString(R.string.meeting_rejected, "You")
                 }
             }
-            holder.timestamp!!.text = android.text.format.DateUtils.getRelativeTimeSpanString(request.meetingRequest?.timestamp?.time ?: 0)
+            MeetingType.ACTIVE -> {
+                if (request.rowType == RowType.DELETABLE) {
+                    holder.userName!!.text = context.getString(
+                        R.string.meeting_active,
+                        request.meetingRequest?.receiverUsername
+                    )
+                } else {
+                    holder.userName!!.text = context.getString(
+                        R.string.meeting_active,
+                        request.meetingRequest?.requesterUsername
+                    )
+                }
+            }
+            MeetingType.INCOMING -> {
+                holder.userName!!.text = context.getString(
+                    R.string.meeting_incoming,
+                    request.meetingRequest!!.requesterUsername
+                )
+                holder.primaryButton?.setOnClickListener { listener.acceptRequest(request.meetingRequest) }
+                holder.secondaryButton?.setOnClickListener { listener.declineRequest(request.meetingRequest) }
+                holder.secondaryButton?.visibility = View.VISIBLE
+            }
+            MeetingType.OUTGOING -> {
+                holder.userName!!.text = context.getString(
+                    R.string.meeting_outgoing,
+                    request.meetingRequest!!.receiverUsername
+                )
+                holder.icon?.visibility = View.VISIBLE
+                holder.primaryButton?.text = context.getString(R.string.cancel)
+                holder.primaryButton?.backgroundColor = context.getColor(R.color.color_warning)
+                holder.primaryButton?.setOnClickListener { listener.deleteRequest(request.meetingRequest) }
+            }
+        }
+        holder.timestamp!!.text = android.text.format.DateUtils.getRelativeTimeSpanString(
+            request.meetingRequest?.timestamp?.time ?: 0
+        )
+        if (request.expanded) {
+            holder.expandedLayout?.visibility = View.VISIBLE
+            holder.card?.elevation = 8.0f
         } else {
-            holder.headerTxt!!.text = context.getString(R.string.meeting_heading, request.type.toString())
+            holder.expandedLayout?.visibility = View.GONE
+            holder.card?.elevation = 0f
         }
     }
 
@@ -75,10 +141,22 @@ class MeetingRequestsAdapter(
     }
 }
 
+interface MeetingRequestViewListener {
+    fun showOnMap(meetingRequest: MeetingRequest)
+    fun acceptRequest(meetingRequest: MeetingRequest)
+    fun declineRequest(meetingRequest: MeetingRequest)
+    fun deleteRequest(meetingRequest: MeetingRequest)
+}
+
 class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     // Holds the TextView that will add each animal to
     val userName: TextView? = view.receiver_txt
-    val meetBtn: Button? = view.meet_btn
+    val primaryButton: MaterialButton? = view.primary_btn
+    val secondaryButton: MaterialButton? = view.secondary_btn
+    val expandedLayout: ConstraintLayout? = view.expanded_layout
     val timestamp: TextView? = view.timestamp_txt
     val headerTxt: TextView? = view.header_txt
+    val card: MaterialCardView? = view.card
+    val icon: ImageView? = view.icon_view
+    val topDivider: View? = view.header_divider
 }
